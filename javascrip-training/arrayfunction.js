590a3f899c985fd37a0f5347d78f3cddadd85ccd
rahuljain <rahuljain.qaindia@gmail.com>
2026-08-26 22:44:43 +0530

    rahuljainqa

---
+++ javascrip-training/arrayfunction.js
@@ -0,0 +1,29 @@
+// // Creating array
+// let numbers = [2, 4 , 8, 12, 16];
+// //Printing array
+// console.log(numbers);
+
+
+
+// // Creating an Empty Array
+// let a = [];
+// console.log(a);
+
+// // Creating an Array and Initializing with Values
+// let b = [10, 20, 30];
+// console.log(b);
+
+
+// // Creating and Initializing an array with values
+// let a = new Array(10, 20, 30);
+
+// console.log(a);
+
+
+
+
+//1. Create an array of strings
+const fruits = ['apple', 'banana', 'cherry'];
+//let firstFruit = fruits[0];  // Accesses the first item ('apple')
+let lastFruit = fruits[fruits.length];  // Accesses the last item ('cherry')
+console.log(lastFruit);      // doubt how to cherry
